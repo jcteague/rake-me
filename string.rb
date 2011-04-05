6db96113f744fba46ec5141f0f6e6534b8651f62
Alexander Groß <agross@therightstuff.de>
2011-04-05 14:49:25 +0200

    Extending string extensions for file handling

--- string.rb
+++ string.rb
@@ -18,4 +18,14 @@ class String
 	def to_absolute()
 		File.expand_path(self)
 	end
+	
+	def exist?
+		if File.exist?(self)
+			self
+		end
+	end
+
+	def patherize
+		self.gsub(/_/, ' ').gsub(/\b('?[a-z])/) { $1.capitalize }.gsub(/\s/, '')
+	end
 end
